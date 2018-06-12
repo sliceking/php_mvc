04085f2f99c7bb051b71a7b71ddb5f9aed128929
stan wielga <svwielga4@gmail.com>
2018-06-12 16:55:17 -0700

    implemented insert on users table

--- controllers/add-name.php
+++ controllers/add-name.php
@@ -1,3 +1,9 @@
 <?php
 
-var_dump('you typed ' . $_POST['name']);
+$app['database']->insert('users', [
+
+    'name' => $_POST['name'],
+
+]);
+
+header('Location: /');
--- views/index.view.php
+++ views/index.view.php
@@ -1,5 +1,11 @@
 <?php require 'partials/head.php';?>
 
+    <?php foreach ($users as $user): ?>
+
+        <li><?=$user->name?></li>
+
+    <?php endforeach;?>
+
   <h1>Submit your Name</h1>
 
   <form method="POST" action="/names">
